0f405d54dc440d011b0e698cb88cb44cd95a6d26
Jean-Pierre Both <jeanpierre.both@gmail.com>
2021-09-14 18:36:15 +0200

    added README

---
+++ README.md
@@ -0,0 +1,6 @@
+# A Simple database of primitive genoma
+
+This package (**currently in development**) compute probminhash signature of  bacteria and archaea genomes and stores the id of bacteria and probminhash signature in a Hnsw structure
+
+This package is developped in collaboration with Jianshu Zhao
+
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@
 
  use clap::{App, Arg, SubCommand};
 
+ use std::io;
+ use std::fs::{self, DirEntry};
+ use std::path::Path;
+
 
 // for logging (debug mostly, switched at compile time in cargo.toml)
 use env_logger::{Builder};
@@ -22,6 +26,33 @@ fn init_log() -> u64 {
     println!("\n ************** initializing logger *****************\n");    
     return 1;
 }
+
+
+// returns true if file is a fasta file (possibly gzipped)
+fn is_fasta_file(&DirEntry) -> bool {
+    return false
+}  // end of is_fasta_file
+
+
+
+// scan directory recursively, executing function cb.
+// taken from fd_find
+fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                // check if entry is a fasta.gz file
+                cb(&entry);
+            }
+        }
+    }
+    Ok(())
+}  // end of visit_dirs
+
 
 
  fn main() {
