58cf98a323f86b3dc82172ac60178f9c0064ee5b
Jean-Pierre Both <jeanpierre.both@gmail.com>
2021-10-01 16:53:41 +0200

    begins request module

--- src/bin/request.rs
+++ src/bin/request.rs
@@ -1,5 +1,26 @@
 //! Module request
+//! try to match fasta sequence with repsect to database
+//! 
+//! request -- database [-d] dirname --query [-q] fastafileList --nbsearch [-n] nbanswers
+//! 
+//! - database is the name of directory ontaining hnsw dump files and seqdict dump
+//! - fastaFileList is a file containing list of fasta file containing sequence to search for
+//! - 
+//
+
+
+
+use clap::{App, Arg};
+
+// for logging (debug mostly, switched at compile time in cargo.toml)
+use env_logger::{Builder};
+
+//
+use std::time::{SystemTime};
+use cpu_time::ProcessTime;
+
+use archaea::idsketch::{SeqDict,Id};
 
 fn main() {
-    
+
 }
