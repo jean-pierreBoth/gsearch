a209666c72357f763d44f0749fb21f10b39b5973
Jean-Pierre Both <jeanpierre.both@gmail.com>
2021-10-27 18:34:01 +0200

    added Clone in some struct

--- src/matcher.rs
+++ src/matcher.rs
@@ -1,4 +1,5 @@
-//!  A module to quantify match between sequence
+//! A module to quantify match between sequence.  
+//! 
 //! For each file/genome we maintain a list of matches with other files/genomes
 //! A match between sequences is a couple of sequence one from request genome, one from the database
 //! for which we found a distance below some significant threshold.
@@ -12,23 +13,39 @@ use std::collections::HashMap;
 
 use crate::utils::*;
 
-
-/// A match between 2 sequences.
-/// TODO devise an order
-pub struct  SequenceMatch {
-    request_item : ItemDict,
-    /// database_item
+#[derive(Clone)]
+pub struct Candidate {
     base_item : ItemDict,
     /// jaccard distance computed by hnsw
     distance : f32,
     ///
-    likelyhood : f64,
+    likelyhood : f64,    
+}
+
+
+impl Candidate {
+    pub fn new(item: ItemDict, distance:f32) -> Self {
+        Candidate{ base_item : item, distance : distance, likelyhood : 0.}
+    }
+
+} // end of impl block Candidate
+
+//==================================
+
+/// A match between 2 sequences.
+/// TODO devise an order
+#[derive(Clone)]
+pub struct SequenceMatch {
+    request_item : ItemDict,
+    ///
+    candidates : Vec<Candidate>
 }  // end of MatchSequence
 
-impl <'a> SequenceMatch {
-    pub fn new(request_item : ItemDict, base_item : ItemDict, distance : f32) -> Self {
-        let likelyhood = 0f64; // TODO to compute
-        SequenceMatch{ request_item, base_item, distance, likelyhood}
+
+
+impl SequenceMatch {
+    pub fn new(request_item : ItemDict, candidates : Vec<Candidate>) -> Self {
+        SequenceMatch{ request_item, candidates}
     }
 
 }  // end of SequenceMatch
@@ -42,7 +59,7 @@ pub struct GenomeMatch {
     request_file : String,
     /// target file in which some sequences were found 
     target_file : String,
-    /// a sequence of mathes between the request and a genome in the database.
+    /// a sequence of matches between the request and a genome in the database.
     seq_matches : Vec<SequenceMatch>
 }
 
@@ -53,6 +70,7 @@ type Matches<'a> = HashMap<(String, String), GenomeMatch >;
 
 
 /// This structure quantifies a match
+#[derive(Clone)]
 pub struct Matcher<'a> {
     kmer_size : usize,
     /// Dictionary of Database
@@ -72,5 +90,13 @@ impl <'a> Matcher<'a> {
         Matcher{kmer_size, seqdict, database_size, seq_matches}
     }
 
+    pub fn insert_sequence_match(&mut self, request_item: ItemDict, candidates : Vec<Candidate>) {
+        let new_match = SequenceMatch::new(request_item, candidates);
+        self.seq_matches.push(new_match);
+    }  // end of insert_sequence_match
 
-} // e,d of impl block for Matcher
+    pub fn get_nb_sequence_match(&self) -> usize {
+        self.seq_matches.len()
+    }
+    
+} // end of impl block for Matcher
