9eb42ba8f256eb8492978c03a42a11c7f20ff2b6
Jean-Pierre Both <jeanpierre.both@gmail.com>
2022-01-09 09:34:27 +0100

    fixed DataType for AA insertio

--- src/aa/aasketch.rs
+++ src/aa/aasketch.rs
@@ -65,7 +65,7 @@ fn sketchandstore_dir_compressedkmer<Kmer:CompressedKmerT>(dirpath : &Path, filt
             let start_t_prod = SystemTime::now();
             let res_nb_sent;
             if block_processing {
-                res_nb_sent = process_dir(&mut state, &DataType::DNA, dirpath, filter_params, &process_aafile_in_one_block, &send);
+                res_nb_sent = process_dir(&mut state, &DataType::AA, dirpath, filter_params, &process_aafile_in_one_block, &send);
             }
             else {
                 panic!("processing by concat and split not implemented for rna");
