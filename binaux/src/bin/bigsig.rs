b232fcb744da70f4d9b75c137c112262fc114b59
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2025-06-15 12:31:45 -0700

    Update bigsig.rs
    update version

--- binaux/src/bin/bigsig.rs
+++ binaux/src/bin/bigsig.rs
@@ -19,7 +19,7 @@ fn main() -> Result<()> {
     let _ = init_log();
 
     let matches = Command::new("bigsig")
-        .version("0.1.0")
+        .version("0.2.9")
         .about("Large-scale Sequence Search with BItsliced Genomic Signature Index (BIGSIG)")
         .arg_required_else_help(true)
         .subcommand(Command::new("construct")
